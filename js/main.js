88c94a06f6fc2599557bc57b0b4fa5cec6b68906
Nicole <nicoletu183@gmail.com>
2021-11-19 14:42:50 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+window.onload = function() {
+    lightGallery(document.getElementById('static-thumbnails'), {
+        animateThumb: false,
+        zoomFromOrigin: false,
+        allowMediaOverlap: true,
+        toggleThumb: true,
+    });
+};
