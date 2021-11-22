3090023478700930e1c57b8df415314bbbb3113e
Nicole <nicoletu183@gmail.com>
2021-11-22 11:11:30 +0800

    fix

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
-
-
 window.onload = function() {
-    lightGallery(document.getElementById('static-thumbnails'), {
+    lightGallery(document.getElementById('animation-thumbnails'), {
+        thumbnail: true,
+        selector: "a"
+    });
+    lightGallery(document.getElementById('map-thumbnails'), {
         animateThumb: false,
         zoomFromOrigin: false,
         allowMediaOverlap: true,
         toggleThumb: true,
+        selector: "a"
     });
 };
+
+window.onscroll = function() {
+    mybutton = document.getElementById("topBtn");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+};
+
+function topFunction() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
